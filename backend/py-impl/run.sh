52d660ec5f58e32b97291a764f3203e1bbf4aed5
xicy <xicy@yintong.com.cn>
2018-01-10 13:21:28 +0800

    modify sth

--- backend/py-impl/run.sh
+++ backend/py-impl/run.sh
@@ -1,2 +1,2 @@
-ps aux | grep XchaoServer.py | awk '{print $2}'| xargs kill -9
-nohup python3 XchaoServer.py > test.log &
+ps aux | grep RiskModelServer.py | awk '{print $2}'| xargs kill -9
+nohup python3.6 RiskModelServer.py > test.log &

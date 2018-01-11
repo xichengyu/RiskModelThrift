50f9366c8ed33736328a1e3e41fd8ed17552c35b
xicy <xicy@yintong.com.cn>
2018-01-11 16:33:28 +0800

    modify sth

--- backend/py-impl/RiskModelServer.py
+++ backend/py-impl/RiskModelServer.py
@@ -60,6 +60,7 @@ if __name__ == "__main__":
     cnf_file_path = "../conf/riskmodel.conf"
 
     thrift_server = "172.31.24.11:9090"
+    # thrift_server = "0.0.0.0:9090"
 
     BuildTserver(thrift_server.split(":")[0], int(thrift_server.split(":")[1]))
 

b38ed1918ffc7ed1341383dae16e83d6b11f4003
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2018-01-11 15:06:32 +0800

    modify code

--- backend/py-impl/planner.py
+++ backend/py-impl/planner.py
@@ -11,6 +11,9 @@ import time
 from sklearn.externals import joblib
 
 
+fieldnames = [x.strip() for x in open("./conf/fieldname.txt").readlines()]
+
+
 class data_handler(object):
     def __init__(self, thrift_request, filtering_model):
         self.request = thrift_request
@@ -19,20 +22,27 @@ class data_handler(object):
     def get_data(self):
         X = []
         try:
-            X = joblib.load("./conf/X.nparray")
+            # X = joblib.load("./conf/X.nparray")
+            X_dict = self.request["fieldData"]
+            for fieldname in fieldnames:
+                X.append(X_dict[fieldname])
         except:
-            pass
+            return KeyError
         return X
 
     def gen_score(self):
+        data = self.get_data()
+        if not data:
+            return "request format error!"
         try:
-            return self.model(self.get_data())
+            return self.model(data)
         except:
             print(time.strftime("[%Y-%m-%d %H:%M:%S]", time.localtime()), traceback.format_exc())
-            return []
+            return "request format error!"
 
     def gen_response(self):
-        response = {"score": self.gen_score()}
+        response = {"score": self.gen_score(), "responseId": self.request["queryId"], "idnoHash":self.request["idnoHash"],
+                    "applyDate":self.request["applyDate"]}
         return json.dumps(response)
 
 

e4fd1d7813e61d7d8b9c72f5479b4c7f1f72407b
xicy <xicy@yintong.com.cn>
2018-01-15 10:53:23 +0800

    add run.sh

---
+++ backend/py-impl/run.sh
@@ -0,0 +1,6 @@
+cp ../../../LLpay_RiskModel/ModelBuilder/OfflineBuilder/ScoreCard/conf/woe_score.nparray ./conf/
+cp ../../../LLpay_RiskModel/ModelBuilder/OfflineBuilder/ScoreCard/conf/X_interval.nparray ./conf/
+cp ../../../LLpay_RiskModel/ModelBuilder/OfflineBuilder/ScoreCard/conf/scale_location.coef ./conf/
+ps aux | grep RiskModelServer.py | awk '{print $2}'| xargs kill -9
+nohup python3.6 RiskModelServer.py > test.log &
+

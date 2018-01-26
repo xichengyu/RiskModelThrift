563b12dde42e2b46a9a398a68298cecb8771d3d5
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2018-01-26 11:12:05 +0800

    modify RiskModel.thrift

--- doc/RiskModel.thrift
+++ doc/RiskModel.thrift
@@ -4,6 +4,7 @@
     Request (string value in JSON format):
     KEY                 DESCRIPTION             TYPE
     queryId             query id                string
+    modelId             model id                string
     idnoHash            idno in hash format     string
     applyDate           apply data              string
     fieldData           data of fields          map or dict({})
@@ -12,9 +13,10 @@
     KEY                 DESCRIPTION             TYPE
     fieldName         name of each filed        string
 
-    
+
     {
         "queryId": "1515639695000",(uuid)
+        "modelId": "LL038",
         "idnoHash": "d8cb27acb114052c8274d9ee1af5bc40abf1ee94bdb885e0f1733f8fd7d287c2",(加密的用户idno(string))
         "applyDate": "2017-01-11 11:11:11", (用户申请时间，时间格式(string))
         "fieldData": {"fieldName1": 1.0,
@@ -33,6 +35,7 @@ struct RiskModelRequest
     Response (string value in JSON format):
     KEY                 DESCRIPTION             TYPE
     responseId          response id             string
+    modelId             model id                string
     idnoHash            idno in hash format     string
     applyDate           apply data              string
     score               score of user           float
@@ -40,6 +43,7 @@ struct RiskModelRequest
 
     {
         "responseId": "1515639695000", (uuid)
+        "modelId": "LLo38",
         "idnoHash": "d8cb27acb114052c8274d9ee1af5bc40abf1ee94bdb885e0f1733f8fd7d287c2", (加密的用户idno(string))
         "applyDate": "2017-01-11 11:11:11", (用户申请时间，时间格式(string))
         "score": 1.0, (用户各指标详细数据，支持多用户查询，list格式，指标数据形式为float)

4b704a71c7a4a6f137805711e7c6949ca1643b2b
chengyu xi <chengyuxi@chengyudeMacBook-Pro.local>
2018-03-16 15:38:15 +0800

    modify planner.py

--- backend/py-impl/planner.py
+++ backend/py-impl/planner.py
@@ -26,20 +26,22 @@ class data_handler(object):
         self.model = filtering_model
 
     def get_data(self):
-        # X = np.array([[]])
+        X = np.array([[]])
         try:
             # X_dict = self.request["fieldData"]
             X = df(self.request["fieldData"])
             # for fieldname in fieldname_dict[self.request["modelId"]]:
                 # X = np.column_stack((X, np.array(X_dict[fieldname])))
             X = X[fieldname_dict[self.request["modelId"]]]
+
+            X = X.astype(str)
+            X[np.where(X == "None")] = "-1.0"
+            X = X.astype(float)
+            return np.array(X)
         except:
-            # traceback.print_exc()
+            traceback.print_exc()
             return KeyError
-        X = X.astype(str)
-        X[np.where(X == "None")] = "-1.0"
-        X = X.astype(float)
-        return np.array(X)
+
 
     def gen_score(self):
         data = self.get_data()
